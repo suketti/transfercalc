6a903456a6a3876bf1be1c9825398ece1cf90f5c
sketchy <sketchy@6D56>
2023-09-05 13:10:52 +0200

    プロジェクト ファイルを追加します。

---
+++ transfercalc/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace transfercalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcClick(object sender, RoutedEventArgs e)
+        {
+            var conversions = new Dictionary<string, double>()
+            {
+                {"GBps", 1000 },
+                {"MBps", 8 },
+                {"KBps", 0.008},
+                {"Mbps", 1 },
+                {"Tb", 1000000 },
+                {"Gb", 1000 },
+                {"Mb", 1 },
+            };
+
+            int capacity = Convert.ToInt32(txtCapacity.Text);
+            int transferSpeed = Convert.ToInt32(sliTransferSpeed.Value);
+            double calc = capacity * conversions[cbCapacity.Text] / (transferSpeed * conversions[cbTransfer.Text]);
+            lblTime.Content = calc.ToString("0.00") + " sec";
+
+
+            
+        }
+    }
+}
